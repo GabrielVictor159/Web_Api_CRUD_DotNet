bd9d3923daeab8c03a833e8f26fdfee8cfb55934
GabrielVictor159 <gabrielvictor159487@gmail.com>
2023-06-01 10:56:16 -0300

    refatorando o projeto para o visual studio

--- API/Infraestructure/ApplicationDbContext.cs
+++ API/Infraestructure/ApplicationDbContext.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Web_Api_CRUD.Domain;
 namespace Web_Api_CRUD.Infraestructure
 {
     public class ApplicationDbContext : DbContext
     {
-
-
         public DbSet<Cliente> clientes => Set<Cliente>();
         public DbSet<Pedido> pedidos => Set<Pedido>();
         public DbSet<Produto> produtos => Set<Produto>();
         public DbSet<PedidoProduto> pedidoProdutos => Set<PedidoProduto>();
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql(@"Server=db_pedido; Port=5432; Database=postgres; Uid=postgres; Pwd=postgres;");
+            var connectionString = Environment.GetEnvironmentVariable("DBCONN");
+            optionsBuilder.UseNpgsql (connectionString, options =>
+            {
+                options.MigrationsHistoryTable("_MigrationHistory", "Ecommerce");
+                AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            });
+
+            base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
